9612642eabb60527352991f0d6f216a14fcf2b2f
Adib Ahsan Chowdhury <adibc@hsenidmobile.com>
2021-04-05 11:55:38 +0600

    feat(app): websocket inclusion

--- App.js
+++ App.js
@@ -4,12 +4,34 @@ import {StyleSheet, Text, View, Button, ToastAndroid} from 'react-native';
 import BackgroundTimer from 'react-native-background-timer';
 import axios from "axios";
 import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+import { setWsHeartbeat } from "ws-heartbeat/client";
+
 
 
 export const axiosInstance = axios.create({
-    baseURL: "http://192.168.1.192:8081" + "/riders-api",
+    baseURL: "https://dadelivery.appigo.co" + "/riders-api",
     responseType: "json",
 });
+
+
+const webSocketNotifications = () =>{
+    ReactNativeForegroundService.remove_task("taskid")
+
+
+    ReactNativeForegroundService.add_task(() => console.log("I am Being Webhooked"), {
+        delay: 10000,
+        onLoop: false,
+        taskId: "WEBSOCKET_4255",
+        onSuccess: res => wsSetup(),
+        onError: (e) => console.log(`Error logging:`, e),
+    });
+
+    ReactNativeForegroundService.start({
+        id: 444,
+        title: 'WebSocket Service',
+        message: 'you are online!',
+    }).then(r => console.log("WebScoket Service",r))
+}
 
 
 ReactNativeForegroundService.add_task(() => console.log("I am Being Tested"), {
@@ -20,6 +42,21 @@ ReactNativeForegroundService.add_task(() => console.log("I am Being Tested"), {
     onError: (e) => console.log(`Error logging:`, e),
 });
 
+ReactNativeForegroundService.remove_task()
+
+
+const wsSetup = () =>{
+    console.log("Setupping WebSocket");
+    const ws = new WebSocket("wss://974c23b8a17a.ngrok.io/web-socket");
+
+    console.log("WebSocket is WebSocket",ws);
+
+    setWsHeartbeat(ws, '{"Andrew":"ping"}',{
+        pingTimeout: 60000, // in 60 seconds, if no message accepted from server, close the connection.
+        pingInterval: 10000, // every 25 seconds, send a ping message to the server.
+    });
+}
+
 const syncFunction = () =>{
     axiosInstance.get("/sync", {
             headers: {
@@ -27,7 +64,7 @@ const syncFunction = () =>{
             },
         },
     ).then((response) => {
-        console.log("Sync OK");
+        console.log("Sync OK" , response.data);
         if (response.status === 200) {
             ToastAndroid.showWithGravity(
                 "Connection OK",
@@ -101,6 +138,27 @@ export default function App() {
                     }/>
                 </View>
             </View>
+
+            <View style={styles.containerTwo}>
+                <View style={styles.buttonContainer}>
+                    <Button onPress={ () =>{
+
+                        console.log("Starting Websocket Service");
+                        webSocketNotifications();
+                    }
+                    } title="Websocket Task"/>
+                </View>
+                <View  style={styles.buttonContainer}>
+                    <Button color ="red" title="Off" onPress={
+                        ()=>{
+                            ReactNativeForegroundService.stop().then(
+                                r => console.log("Stopping Service",r)
+                            )
+                        }
+
+                    }/>
+                </View>
+            </View>
             <StatusBar style="auto"/>
         </View>
     );
@@ -131,6 +189,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: -20,
+        marginBottom: -20
     },
     buttonContainer: {
         flex: 1,
